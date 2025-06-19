1367642fec505eb3396c086ff1313c59ce35f0ad
Thomas Collart <thomas.collart@gmail.com>
2025-06-19 12:37:43 +0200

    Add variables.c

---
+++ variables.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main (void) {
+    int entier = 10;
+    float flottant = 3.14;
+    char caractere = 'A';
+
+    printf("Entier : %d, Flottant : %.2f, Char : %c\n", entier, flottant, caractere);
+
+    return 0;
+}
